a372f79a294394b22fd18419fc57bfda85b10a22
Yiming Pu <pymmmmmm@qq.com>
2025-03-13 21:25:52 +0800

    完成功能： 主数据管理-组织管理

---
+++ src/api/organization/organization.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询组织管理列表
+export function listOrganization(query) {
+    return request({
+        url: '/organization/organization/list',
+        method: 'get',
+        params: query
+    })
+}
+
+// 查询组织管理详细
+export function getOrganization(id) {
+    return request({
+        url: '/organization/organization/' + id,
+        method: 'get'
+    })
+}
+
+// 新增组织管理
+export function addOrganization(data) {
+    return request({
+        url: '/organization/organization',
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改组织管理
+export function updateOrganization(data) {
+    return request({
+        url: '/organization/organization',
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除组织管理
+export function delOrganization(id) {
+    return request({
+        url: '/organization/organization/' + id,
+        method: 'delete'
+    })
+}
